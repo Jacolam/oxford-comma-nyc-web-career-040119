df72ef8a1005381eae7d910452697939f50bf10e
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-17 18:51:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,3 @@
 def oxford_comma(array)
-  
-  if array.length ==  2  
-    
     array.join(" and ")
-    
-  elsif array.length > 2 
-    
-    array.insert(array.last - 1, " and ")
-  end 
-
 end
